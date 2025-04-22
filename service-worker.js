a5f7108a2788f992371598f7065f036d8b4850f8
shaikafrida <shaikafrida52@gmail.com>
2025-04-22 12:23:47 +0530

    Update service-worker.js

--- service-worker.js
+++ service-worker.js
@@ -22,5 +22,7 @@ self.addEventListener("fetch", event => {
   event.respondWith(
     caches.match(event.request)
       .then(response => response || fetch(event.request))
-  );
+    });
+self.addEventListener('activate', event => {
+  console.log('Service Worker activated!');
 });
